ede5c019c1dd6d4c756d5eac6d2984f3018a24b7
Ольга <o.filippova@movavi.com>
2022-04-03 17:04:20 +0700

    del comments

--- src/frontend/src/common/components/ItemCounter.vue
+++ src/frontend/src/common/components/ItemCounter.vue
@@ -32,8 +32,6 @@ export default {
   data() {
     return {
       inputValue: 0,
-      // ingredientValue: "",
-      // dataIngredients: [],
     };
   },
   methods: {
@@ -44,67 +42,13 @@ export default {
       );
     },
     plus() {
-      //console.log(ingredient);
       ++this.inputValue;
-      // this.ingredientValue = ingredient;
-      // if (!this.dataIngredients.length) {
-      //   this.dataIngredients.push({
-      //     value: ingredient,
-      //     count: this.inputValue,
-      //   });
-      // } else {
-      //   this.dataIngredients.map((post) =>
-      //     post.value === ingredient
-      //       ? (post.count = this.inputValue)
-      //       : this.dataIngredients.push({
-      //           value: ingredient,
-      //           count: this.inputValue,
-      //         })
-      //   );
-      //   console.log("this.dataIngredients", this.dataIngredients);
-      // }
-      // const newPosts = posts.map((post) => (
-      //   post.id === 1
-      //     ? { ...post, text: 'other text' }
-      //     : post
-      // ));
-      //console.log("this.dataIngredients", this.dataIngredients);
-      // this.dataIngredients[this.inputValue] = ingredient;
-      // return this.inputValue;
-      //return { value: ingredient, count: this.inputValue };
       return this.inputValue;
     },
     minus() {
       --this.inputValue;
-      // this.ingredientValue = ingredient;
-      // // this.dataIngredients = this.dataIngredients.filter(function (f) {
-      // //   return f !== ingredient;
-      // // });
-      // this.dataIngredients.map((post) =>
-      //   post.value === ingredient ? (post.count = this.inputValue) : post
-      // );
-      // console.log("this.dataIngredients", this.dataIngredients);
-      // console.log(
-      //   "this.filterByProp(this.dataIngredients, ingredient)",
-      //   this.filterByProp(this.dataIngredients, ingredient)
-      // );
-      // this.filterByProp(this.dataIngredients, ingredient);
-      // return this.inputValue;
       return this.inputValue;
     },
-    // filterByProp(arr, prop) {
-    //   const seen = {};
-    //   return arr.filter((item) => {
-    //     console.log("item", item);
-    //     console.log(seen[item[prop]]);
-    //     if (seen[item[prop]]) {
-    //       return false;
-    //     } else {
-    //       seen[item[prop]] = true;
-    //       return true;
-    //     }
-    //   });
-    // },
   },
 };
 </script>
